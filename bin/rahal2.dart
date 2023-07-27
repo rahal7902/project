9b20bd077bc99a99ca99b2333c439103a2b8d950
aswinachuvappat <aswin2001vappat@gmail.com>
2023-07-27 11:40:00 +0530

    commit

---
+++ bin/rahal2.dart
@@ -0,0 +1,27 @@
+void main(){
+  String name= "rahal";
+  String mandan= "pottan";
+  int age = 26;
+  int a=10;
+  int b=4;
+  double mark=78.6;
+  bool pass=true;
+  var school="6777";
+  school="234";
+  school="677";
+dynamic college="mtc";
+college=456;
+  dynamic college1="kjh";
+  college1="ggkk";
+  print(college1);
+print(college);
+  print(school);
+  print("name");
+  print("value = ${a+b}");
+  print("value = $a+$b");
+  print("$mandan");
+print("my name is $name my age is $age");
+print("my mark $mark");
+print("my name is rahal");
+print("poochengal \n pallipadi");
+}
